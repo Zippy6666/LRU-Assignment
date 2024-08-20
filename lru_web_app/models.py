494a1e1d6c744def5934114ff4b56a5fdbd72c3c
Zippy6666 <simperzippy@gmail.com>
2024-08-20 11:27:13 +0200

    created person model + migrated

--- lru_web_app/models.py
+++ lru_web_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    person_number = models.CharField(max_length=12)
+    city = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)
+    job = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=15)
