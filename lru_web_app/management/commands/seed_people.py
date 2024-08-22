da8f5fd44bac8a234505a6001af0a93ba598400b
Zippy6666 <simperzippy@gmail.com>
2024-08-22 10:01:40 +0200

    renamings

--- lru_web_app/management/commands/seed_people.py
+++ lru_web_app/management/commands/seed_people.py
@@ -1,27 +1,24 @@
 from django.core.management.base import BaseCommand
 from lru_web_app.models import Person
 from faker import Faker
-from threading import Thread # TODO
 
 
 SEED_DATA_COUNT: int = 1_000_000
 BATCH_SIZE: int = SEED_DATA_COUNT//100
 
 
-def to_person_number(num: int) -> str:
+def to_pn(num: int) -> str:
     num = str(num)
-    personnummer = f"{num[0:8]}-{num[8:12]}"
-    return personnummer
+    pn = f"{num[0:8]}-{num[8:12]}"
+    return pn
 
 
 class Command(BaseCommand):
-    help = "Seeds data into the database"
+    help = "Seeds people into the database."
 
-    def handle(self, *args, **kwargs):
-        self.stdout.write(self.style.NOTICE(f"Trying to seed data... {args} {kwargs}"))
-
+    def handle(self, *_, **__):
         Person.objects.all().delete()
-        self.stdout.write(self.style.NOTICE('Removed any old "Person" instances...'))
+        self.stdout.write(self.style.NOTICE('Removed any old instances...'))
 
         fake = Faker()
         objects = []
@@ -29,7 +26,7 @@ class Command(BaseCommand):
         bulk_creations = 0
         for _ in range(SEED_DATA_COUNT):
             city = fake.city()
-            person_number = to_person_number(
+            pn = to_pn(
                 fake.random_number(digits=12, fix_len=True)
             )
             name = fake.name()
@@ -41,7 +38,7 @@ class Command(BaseCommand):
                 job=job,
                 country=country,
                 phone_number=phone_number,
-                person_number=person_number,
+                pn=pn,
                 city=city,
             )
             objects.append(person)
@@ -55,10 +52,10 @@ class Command(BaseCommand):
 
         first_person = Person.objects.first()
         name = first_person.name
-        person_number = first_person.person_number
+        pn = first_person.pn
         phone_number = first_person.phone_number
         self.stdout.write(
             self.style.SUCCESS(
-                f"Sucess! Example person: {name} {person_number} {phone_number}"
+                f"Sucess! Example person: {name}, pn: {pn}, phone number: {phone_number}"
             )
         )
--- lru_web_app/models.py
+++ lru_web_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField(max_length=30)
-    person_number = models.CharField(max_length=12)
+    pn = models.CharField(max_length=12)
     city = models.CharField(max_length=30)
     country = models.CharField(max_length=30)
     job = models.CharField(max_length=30)
