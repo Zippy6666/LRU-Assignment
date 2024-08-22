b8bec6c30f43d761af8446a0974ce4f77e855efb
Zippy6666 <simperzippy@gmail.com>
2024-08-22 20:34:44 +0200

    tests + comments

--- lru_web_app/my_lru.py
+++ lru_web_app/my_lru.py
@@ -2,17 +2,20 @@ from typing import Callable, Any
 
 
 class LRUCache:
-    class _CachedInstance:
+    max = 50
+
+    class _CachedReturnValue:
         def __init__(self, args: tuple, return_value: Any) -> None:
             self.args = args
             self.return_value = return_value
 
         def __repr__(self) -> str:
-            return str( self.return_value )
+            return str(self.return_value)
 
-    def __init__(self, max_: int) -> None:
-        self._cache_list: list[self._CachedInstance] = []
-        self._max: int = max_
+    def __init__(self, func: Callable) -> None:
+        """My half-decent LRU-cache. Only caches return-values by functions that have the same positional arguments (in the same order). The cache is limited to 50 return-values."""
+        self._func = func
+        self._cache_list: list[self._CachedReturnValue] = []
 
     def _is_cached(self, args: tuple) -> tuple[Any, Any]:
         for position, instance in enumerate(self._cache_list):
@@ -20,45 +23,46 @@ class LRUCache:
                 return instance.return_value, position
         return False, False
 
-    def __call__(self, func: Callable, *_) -> Any:
-        def wrapper(*args):
-            cached_return_value, position = self._is_cached(args)
-            if not cached_return_value is False:
-                # Move this value to the first position in the list
-                self._cache_list[0], self._cache_list[position] = (
-                    self._cache_list[position],
-                    self._cache_list[0],
-                )
+    def __call__(self, *args, **_) -> Any:
+        cached_return_value, position = self._is_cached(args)
+        if not cached_return_value is False:
+            # Move this value to the first position in the list
+            self._cache_list[0], self._cache_list[position] = (
+                self._cache_list[position],
+                self._cache_list[0],
+            )
 
-                print(self._cache_list)
+            # print(self._cache_list)
+            # print(cached_return_value, "found in cache, returning")
 
-                # Is cached, so return cached value
-                return cached_return_value
+            # Is cached, so return cached value
+            return cached_return_value
 
-            return_value = func(*args)
+        return_value = self._func(*args)
 
-            # Insert at first index
-            self._cache_list.insert(0, self._CachedInstance(args, return_value))
-            if len(self._cache_list) > self._max:
-                self._cache_list.pop() # Remove last element if cache it too large
+        # Insert at first index
+        self._cache_list.insert(0, self._CachedReturnValue(args, return_value))
+        if len(self._cache_list) > self.max:
+            self._cache_list.pop()  # Remove last element if cache is larger than 50
 
-            print(self._cache_list)
+        # print(self._cache_list)
+        # print(return_value, "not found in cache")
 
-            return return_value
-        return wrapper
+        return return_value
 
 
 if __name__ == "__main__":
     import time
 
-    @LRUCache(3)
-    def expensive_operation(a, b):
+    @LRUCache
+    def expensive_operation(a, b, **kwargs):
         time.sleep(2)
         return a * b
 
-    expensive_operation(3, 3)
+    expensive_operation(3, 3, test="123")
     expensive_operation(3, 3)
     expensive_operation(5, 3)
     expensive_operation(4, 7)
     expensive_operation(6, 1)
     expensive_operation(3, 3)
+    expensive_operation(6, 1)
